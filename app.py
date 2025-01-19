bee1fbf478da5a7ade3dcb41ed37f3d6fa662e7d
Rochakjain05 <rochakjain510@gmail.com>
2025-01-20 01:35:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def extract_medicines():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5005)
+    app.run(host='0.0.0.0', port=5000)
